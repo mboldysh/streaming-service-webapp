d430df2161681f3a59d14417b7caf559dc556465
mboldysh <maksym.boldysh@gmail.com>
2018-09-23 19:28:14 +0300

    Add editconfig and eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    'env': {
+      'browser': true,
+      'jest': true,
+      'es6': true,
+      'node': true,
+    },
+    'extends': [
+      'airbnb',
+      'plugin:prettier/recommended',
+      'prettier/react',
+      'prettier/standard'
+    ],
+    'plugins': [
+      'prettier',
+    ],
+    'rules': {
+      'prettier/prettier': ['error', {
+        'singleQuote': true,
+        'trailingComma': 'es5'
+      }],
+    },
+    'parserOptions': {
+      'ecmaFeatures': {
+        'jsx': true,
+      }
+    }
+  }
